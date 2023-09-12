c78dc6f78813b6ae8196b0e1ab404ceeab6c32f6
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2023-09-12 14:56:45 +0500

    Изменил метод add_new_fishes в соответствии с новым классом ListFish

--- pool.py
+++ pool.py
@@ -1,4 +1,4 @@
-from fish import Fish
+from fish import Fish, ListFish
 
 
 class Pool:
@@ -31,16 +31,18 @@ class Pool:
             self.average_mass = self.biomass / self.number_fish * 1000  # в граммах
             self.planting_density = self.biomass / self._area
 
-    def add_new_fishes(self, new_fishes: list[Fish]):
+    def add_new_fishes(self, new_fish: ListFish):
         """
         Метод для добавления новых рыбок в бассейн.
-        :param new_fishes: Список новых рыбок.
-        :return: Ничего
+        :param new_fish: Список новых рыбок.
+        :return: Ничего.
         """
-        for fish in new_fishes:
+        for fish in new_fish.list_fish:
             self.fishes.append(fish)
-            self.biomass += fish.mass / 1000
-            self.number_fish += 1
+
+        # Обновим информацию о рыбе в бассейне
+        self.biomass += new_fish.get_biomass()
+        self.number_fish += new_fish.get_number_fish()
 
         # Отсортируем рыбу по массе
         self.fishes.sort(key=lambda item: item.mass)
@@ -108,43 +110,3 @@ class Pool:
               f'Количество рыбы в бассейне: {self.number_fish} шт.\n'
               f'Средняя масса рыбы: {self.average_mass} г.\n'
               f'Плотность посадки: {self.planting_density} кг/м^2.')
-
-
-"""
-number_fish: int = 100
-start_mass: float = 100.0
-feed_ratio: float = 1.5
-
-fishes: list[Fish] = list()
-for _ in range(number_fish):
-    fishes.append(Fish(start_mass, feed_ratio))
-
-pool: Pool = Pool(6.0)
-
-pool.print()
-pool.add_new_fishes(fishes)
-pool.print()
-print()
-
-days: int = 100
-increase_biomass: float = 0.0
-spent_feed: float = 0.0
-
-for _ in range(days):
-    result: dict[str, float] = pool.daily_growth()
-    increase_biomass += result['biomass_increase']
-    spent_feed += result['spent_feed']
-
-pool.print()
-print(f'increase_biomass = {increase_biomass} kg\n'
-      f'spent_feed = {spent_feed} kg')
-print()
-
-removed_fish1: list[Fish] = pool.remove_fish(10)
-pool.print()
-print()
-
-removed_fish2: list[Fish] = pool.remove_fish(10, biggest_fish=False)
-pool.print()
-print()
-"""
